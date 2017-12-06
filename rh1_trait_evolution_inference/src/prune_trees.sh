96a7e321823c15f714e6dfb7802fe19c056caf63
Michael Matschiner <michaelmatschiner@mac.com>
2017-12-06 13:06:34 +0100

    Compressed .trees file

--- rh1_trait_evolution_inference/src/prune_trees.sh
+++ rh1_trait_evolution_inference/src/prune_trees.sh
@@ -6,6 +6,12 @@ mkdir -p ../res/trees
 # Make a list of tips to keep.
 cat ../data/tables/wavelengths.txt | cut -f 1 > ../data/tables/keep_list.txt
 
+# Uncompress file ../data/trees/rod_opsin.trees.gz.
+home=`pwd`
+cd ../data/trees
+gunzip rod_opsin.trees.gz
+cd ${home}
+
 # Prune all taxa from the tree that are not in the keep list.
 rscript prune_trees.r ../data/trees/rod_opsin.trees ../res/trees/rod_opsin_pruned.trees ../data/tables/keep_list.txt
 rscript prune_tree.r ../data/trees/rod_opsin.tre ../res/trees/rod_opsin_pruned.tre ../data/tables/keep_list.txt
